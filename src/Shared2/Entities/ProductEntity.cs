e305895330c0af9de37e60139c9cbebe883b97dc
Bartłomiej Wiercigroch <landupcmr@gmail.com>
2023-09-23 13:19:35 +0200

    Fix shared

---
+++ src/Shared2/Entities/ProductEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Entities
+{
+    public class ProductEntity : IEntity
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required decimal Price { get; set; }
+    }
+}
